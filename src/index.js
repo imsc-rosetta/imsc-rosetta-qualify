55deab0957a6087496e3d2e03e0fcfa7887bb8dd
Simon Hailes <simon@yellaumbrella.tv>
2022-08-18 15:33:06 +0100

    remove debug console.logs, and format code files.

--- src/index.js
+++ src/index.js
@@ -24,65 +24,64 @@ rootparts.pop();
 rootparts.push('');
 let root = rootparts.join('/');
 
-
 var s1 = document.createElement('script');
-s1.setAttribute('src', root+"lib/vue.min.js");
+s1.setAttribute('src', root + "lib/vue.min.js");
 
 document.head.appendChild(s1);
 
 s1 = document.createElement('script');
-s1.setAttribute('src', root+"lib/httpVueLoader.js");
+s1.setAttribute('src', root + "lib/httpVueLoader.js");
 document.head.appendChild(s1);
 
 
-window.onload = ()=>{
+window.onload = () => {
 
-    console.log(window.xml2js);
-    console.log(window.xmlbuilder);
+  //console.log(window.xml2js);
+  //console.log(window.xmlbuilder);
 
-    var appdiv = document.createElement('div');
-    appdiv.setAttribute('id',"app");
-    appdiv.id = 'app';
-    appdiv.style.height = "100vh";
-    document.body.appendChild(appdiv);
-    let comp = document.createElement('imsc-rosetta-qualify');
-    appdiv.appendChild(comp);
-    
-    window.components = [
-        { name:'display', path: root+'vue/display.vue' },
-        { name:'render', path: root+'vue/render.vue' },
-        { name:'imsc-rosetta-qualify', path: root+'vue/imsc-rosetta-qualify.vue' },
-    ];
+  var appdiv = document.createElement('div');
+  appdiv.setAttribute('id', "app");
+  appdiv.id = 'app';
+  appdiv.style.height = "100vh";
+  document.body.appendChild(appdiv);
+  let comp = document.createElement('imsc-rosetta-qualify');
+  appdiv.appendChild(comp);
 
-    let numcomponents = 0;
-    
-    let addComponent = ()=>{
-        let component = httpVueLoader(window.components[numcomponents].path);
-        component().then( data =>{
-            window.components[numcomponents].data = data;
-            numcomponents++;
-            if (numcomponents < window.components.length){
-                addComponent();
-            } else {
-                new Vue({
-                    el: '#app',
-                    components: {
-                        'imsc-rosetta-qualify': window.components[numcomponents-1].data
-                    }
-                });
-            }
+  window.components = [
+    { name: 'display', path: root + 'vue/display.vue' },
+    { name: 'render', path: root + 'vue/render.vue' },
+    { name: 'imsc-rosetta-qualify', path: root + 'vue/imsc-rosetta-qualify.vue' },
+  ];
+
+  let numcomponents = 0;
+
+  let addComponent = () => {
+    let component = httpVueLoader(window.components[numcomponents].path);
+    component().then(data => {
+      window.components[numcomponents].data = data;
+      numcomponents++;
+      if (numcomponents < window.components.length) {
+        addComponent();
+      } else {
+        new Vue({
+          el: '#app',
+          components: {
+            'imsc-rosetta-qualify': window.components[numcomponents - 1].data
+          }
         });
+      }
+    });
+  }
+
+  window.getComponent = (name) => {
+    for (let i = 0; i < numcomponents; i++) {
+      if (window.components[i].name === name) {
+        return window.components[i].data;
+      }
     }
-    
-    window.getComponent = (name)=>{
-        for (let i = 0; i < numcomponents; i++){
-            if (window.components[i].name === name){
-                return window.components[i].data;
-            }
-        }
-        return undefined;
-    }
-    addComponent();
+    return undefined;
+  }
+  addComponent();
 };
 
 

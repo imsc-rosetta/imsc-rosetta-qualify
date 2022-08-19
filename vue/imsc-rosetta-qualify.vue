b3b7dc14936990cf6a9705fe92ae2d6b22d9695a
btsimonh <simon@yellaumbrella.tv>
2022-08-19 09:28:08 +0000

    Deploying to gh-pages from @ imsc-rosetta/imsc-rosetta-qualify@b929e38582d5ad1bdf1b77836a7cd543a9316d87 🚀

--- vue/imsc-rosetta-qualify.vue
+++ vue/imsc-rosetta-qualify.vue
@@ -16,217 +16,300 @@
 
 <template>
   <div class="fullscreen">
-    <div class="drop" @drop="dropHandler($event)" @dragover="dragOverHandler($event)">
-      <div class="droptext center"><p>Drop .imscr File Here</p><p>{{xmlfilename}}</p></div>
+    <div
+      class="drop"
+      @drop="dropHandler($event)"
+      @dragover="dragOverHandler($event)"
+    >
+      <div class="droptext center">
+        <p>Drop .imscr File Here</p>
+        <p>{{ xmlfilename }}</p>
+      </div>
     </div>
     <!-- Tab links -->
     <div class="tab">
-      <button :class="'tablinks '+Qualifyclass" @click="open($event, 'Qualify')">Qualify File</button>
-      <button :class="'tablinks '+Renderclass" @click="open($event, 'Render')">Render File</button>
-      <button :class="'tablinks '+Aboutclass" @click="open($event, 'About')">About</button>
+      <button
+        :class="'tablinks ' + Qualifyclass"
+        @click="open($event, 'Qualify')"
+      >
+        Qualify File
+      </button>
+      <button
+        :class="'tablinks ' + Renderclass"
+        @click="open($event, 'Render')"
+      >
+        Legacy Render File
+      </button>
+      <button
+        :class="'tablinks ' + RenderPatchedclass"
+        @click="open($event, 'RenderPatched')"
+      >
+        Patched Render File
+      </button>
+      <button :class="'tablinks ' + Aboutclass" @click="open($event, 'About')">
+        About
+      </button>
     </div>
-    
+
     <!-- Tab content -->
-    <div class="tabcontent" :style="(tab === 'Qualify')?null:'display:none;'">
+    <div class="tabcontent" :style="tab === 'Qualify' ? null : 'display:none;'">
       <display-controller @mounted="mountedQualify"></display-controller>
     </div>
-    
-    <div class="tabcontent" :style="(tab === 'Render')?null:'display:none;'">
+
+    <div class="tabcontent" :style="tab === 'Render' ? null : 'display:none;'">
       <render-controller @mounted="mountedRender"></render-controller>
     </div>
-    
+
+    <div class="tabcontent" :style="tab === 'RenderPatched' ? null : 'display:none;'">
+      <render-controller @mounted="mountedRenderPatched"></render-controller>
+    </div>
+
     <div class="tabcontent" v-if="tab === 'About'">
       <h3>About</h3>
-      <p>A simple web app to qualify <a href="https://github.com/imsc-rosetta" target="_blank">IMSC-Rosetta</a> by Simon Hailes at <a href="https://yellaumbrella.tv" target="_blank">YellaUmbrella</a></p>
-      <p>This app is a pure javascript application written in VueJS using SFC .vue components.</p>
-      <p>The XML parser used is <a href="https://github.com/Leonidas-from-XIV/node-xml2js" target="_blank">xml2js</a> - the most popular javascript XML library by far.<br/>
-      The XML output is created using <a href="https://github.com/oozcitak/xmlbuilder-js" target="_blank">xmlbuilder</a> - the most popular javascript XML output library.<br/>
-      IMSC-rosetta enables the use of simpler XML parsers without complex and comprehensive parsing by ensuring certain aspects of XML are constrained.</p>
-      <p>Render is by <a href="https://github.com/sandflow/imscJS" target="_blank">imscJS</a>, Pierre's very competent and comprehensive IMSC renderer.</p>
-    </div>    
-
+      <p>
+        A simple web app to qualify
+        <a href="https://github.com/imsc-rosetta" target="_blank"
+          >IMSC-Rosetta</a
+        >
+        by Simon Hailes at
+        <a href="https://yellaumbrella.tv" target="_blank">YellaUmbrella</a>
+      </p>
+      <p>
+        This app is a pure javascript application written in VueJS using SFC
+        .vue components.
+      </p>
+      <p>
+        The XML parser used is
+        <a
+          href="https://github.com/Leonidas-from-XIV/node-xml2js"
+          target="_blank"
+          >xml2js</a
+        >
+        - the most popular javascript XML library by far.<br />
+        The XML output is created using
+        <a href="https://github.com/oozcitak/xmlbuilder-js" target="_blank"
+          >xmlbuilder</a
+        >
+        - the most popular javascript XML output library.<br />
+        IMSC-rosetta enables the use of simpler XML parsers without complex and
+        comprehensive parsing by ensuring certain aspects of XML are
+        constrained.
+      </p>
+      <p>
+        Render is by
+        <a href="https://github.com/sandflow/imscJS" target="_blank">imscJS</a>,
+        Pierre's very competent and comprehensive IMSC renderer.
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-  module.exports = {
+module.exports = {
   components: {
-    'display-controller': window.getComponent('display'),
-    'render-controller': window.getComponent('render'),
+    "display-controller": window.getComponent("display"),
+    "render-controller": window.getComponent("render"),
   },
-      
-    data: ()=>{
-      return {
-        msg: 'world!',
-        color: 'blue',
-        logs: '',
-        tab: 'Qualify',
-        Renderclass:'',
-        Qualifyclass:'active', // default to this tab
-        Aboutclass:'',
-        xmlfilename: 'none',
+
+  data: () => {
+    return {
+      msg: "world!",
+      color: "blue",
+      logs: "",
+      tab: "Qualify",
+      Renderclass: "",
+      RenderPatchedclass: "",
+      Qualifyclass: "active", // default to this tab
+      Aboutclass: "",
+      xmlfilename: "none",
+    };
+  },
+  methods: {
+    open(evt, name) {
+      console.log("select tab " + name);
+      this.tab = name;
+      this.Qualifyclass = "";
+      this.RenderPatchedclass = "";
+      this.Renderclass = "";
+      this.Aboutclass = "";
+      this[name + "class"] = "active";
+    },
+
+    mountedQualify(name, ptr) {
+      //console.log('mountedQualify ', name, ptr);
+      if (ptr.init) {
+        ptr.init(this.appScope);
+        // if it returns a name, then add this 'class' to our this.
+      }
+      if (name) {
+        this[name] = ptr;
+        //console.log('added child \'class\' '+name);
       }
     },
-    methods:{
-      open(evt, name) {
-          console.log('select tab '+name);
-          this.tab = name;
-          this.Qualifyclass = '';
-          this.Renderclass = '';
-          this.Aboutclass = '';
-          this[name+'class'] = 'active';
-      },       
 
-      mountedQualify( name, ptr ) {
-        console.log('mountedQualify ', name, ptr);
-        if (ptr.init){
-          ptr.init(this.appScope);
-          // if it returns a name, then add this 'class' to our this.
-        }
-        if (name){
-          this[name] = ptr;
-          console.log('added child \'class\' '+name);
-        }
-      },
-
-      mountedRender( name, ptr ) {
-        console.log('mountedRender ', name, ptr);
-        if (ptr.init){
-          ptr.init(this.appScope);
-          // if it returns a name, then add this 'class' to our this.
-        }
-        if (name){
-          this[name] = ptr;
-          console.log('added child \'class\' '+name);
-        }
-      },
-
-      dropHandler(ev){
-          ev.preventDefault();
-          console.log('drop');
-          if (ev.dataTransfer.items) {
-              // Use DataTransferItemList interface to access the file(s)
-              for (var i = 0; i < ev.dataTransfer.items.length; i++) {
-                  // If dropped items aren't files, reject them
-                  if (ev.dataTransfer.items[i].kind === 'file') {
-                      var file = ev.dataTransfer.items[i].getAsFile();
-                      console.log('... file[' + i + '].name = ' + file.name);
-                      reader = new FileReader();
-                      reader.onload = (event) => {
-                          console.log(event);
-                          console.log('len:'+event.target.result.length);
-                          if (this.qualify && this.qualify.processXml){
-                            this.xmlfilename = `${file.name} - ${event.target.result.length} characters`;
-                            this.qualify.processXml(file, event.target.result);
-                          }
-                          if (this.render && this.render.processXml){
-                            this.xmlfilename = `${file.name} - ${event.target.result.length} characters`;
-                            this.render.processXml(file, event.target.result);
-                          }
-                      };
-                      console.log(file);
-                      reader.readAsText(file);
-                  }
-              }
-          } else {
-              // Use DataTransfer interface to access the file(s)
-              for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-                  console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
-                  reader = new FileReader();
-                  reader.onload = (event) => {
-                      console.log(event);
-                      console.log('len:'+event.target.result.length);
-                      if (this.qualify && this.qualify.processXml){
-                        this.xmlfilename = `${file.name} - ${event.target.result.length} characters`;
-                        this.qualify.processXml(file, event.target.result);
-                      }
-                      if (this.render && this.render.processXml){
-                        this.xmlfilename = `${file.name} - ${event.target.result.length} characters`;
-                        this.render.processXml(file, event.target.result);
-                      }
-                  };
-                  console.log(file);
-                  reader.readAsText(file);
-              }
-          }            
-      },
-
-      dragOverHandler(ev){
-          //console.log('File(s) in drop zone');
-          // Prevent default behavior (Prevent file from being opened)
-          ev.preventDefault();
-      },
+    mountedRender(name, ptr) {
+      //console.log('mountedRender ', name, ptr);
+      if (ptr.init) {
+        ptr.init({imsc: window.imsc, version: window.imscVersion, name:'render'});
+        // if it returns a name, then add this 'class' to our this.
+      }
+      if (name) {
+        // creates this.render
+        this[name] = ptr;
+        //console.log('added child \'class\' '+name);
+      }
     },
-    mounted (){
-        this.msg = 'fred';
-        console.log('mounted');
 
-    }
-  }
+    mountedRenderPatched(name, ptr) {
+      //console.log('mountedRender ', name, ptr);
+      if (ptr.init) {
+        ptr.init({imsc: window.imscmod, version: window.imscmodVersion, name:'renderPatched'});
+        // if it returns a name, then add this 'class' to our this.
+      }
+      if (name) {
+        // creates this.renderPatched
+        this[name+'Patched'] = ptr;
+        //console.log('added child \'class\' '+name);
+      }
+    },
+
+    dropHandler(ev) {
+      ev.preventDefault();
+      //console.log('drop');
+      if (ev.dataTransfer.items) {
+        // Use DataTransferItemList interface to access the file(s)
+        for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+          // If dropped items aren't files, reject them
+          if (ev.dataTransfer.items[i].kind === "file") {
+            var file = ev.dataTransfer.items[i].getAsFile();
+            //console.log('... file[' + i + '].name = ' + file.name);
+            reader = new FileReader();
+            reader.onload = (event) => {
+              //console.log(event);
+              //console.log('len:'+event.target.result.length);
+              if (this.qualify && this.qualify.processXml) {
+                this.xmlfilename = `${file.name} - ${event.target.result.length} characters`;
+                this.qualify.processXml(file, event.target.result);
+              }
+              // uses https://unpkg.com/imsc@1.1.0-beta.2/build/umd/imsc.all.min.js
+              if (this.render && this.render.processXml) {
+                this.xmlfilename = `${file.name} - ${event.target.result.length} characters`;
+                this.render.processXml(file, event.target.result);
+              }
+              // uses https://github.com/YellaUmbrella-tv/imscJS/tree/tempmaster
+              // fixes boxing and rubies.
+              if (this.renderPatched && this.renderPatched.processXml) {
+                this.xmlfilename = `${file.name} - ${event.target.result.length} characters`;
+                this.renderPatched.processXml(file, event.target.result);
+              }
+              
+            };
+            //console.log(file);
+            reader.readAsText(file);
+          }
+        }
+      } else {
+        // Use DataTransfer interface to access the file(s)
+        for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+          //console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+          reader = new FileReader();
+          reader.onload = (event) => {
+            //console.log(event);
+            //console.log('len:'+event.target.result.length);
+            if (this.qualify && this.qualify.processXml) {
+              this.xmlfilename = `${file.name} - ${event.target.result.length} characters`;
+              this.qualify.processXml(file, event.target.result);
+            }
+            // uses https://unpkg.com/imsc@1.1.0-beta.2/build/umd/imsc.all.min.js
+            // fixes boxing and rubies.
+            if (this.render && this.render.processXml) {
+              this.xmlfilename = `${file.name} - ${event.target.result.length} characters`;
+              this.render.processXml(file, event.target.result);
+            }
+            // uses https://github.com/YellaUmbrella-tv/imscJS/tree/tempmaster
+            if (this.renderPatched && this.renderPatched.processXml) {
+              this.xmlfilename = `${file.name} - ${event.target.result.length} characters`;
+              this.renderPatched.processXml(file, event.target.result);
+            }
+          };
+          //console.log(file);
+          reader.readAsText(file);
+        }
+      }
+    },
+
+    dragOverHandler(ev) {
+      //console.log('File(s) in drop zone');
+      // Prevent default behavior (Prevent file from being opened)
+      ev.preventDefault();
+    },
+  },
+  mounted() {
+    //console.log('mounted');
+  },
+};
 //@ sourceURL=/vue/imsc-rosetta-qualify.vue
 </script>
 
 <style scoped>
-    .drop {
-        border: 5px solid blue;
-        height: 5%;
-        text-align: center;
-        position: relative;
-        vertical-align: center;
-    }
+.drop {
+  border: 5px solid blue;
+  height: 5%;
+  text-align: center;
+  position: relative;
+  vertical-align: center;
+}
 
-    .droptext p {
-      margin:0;
-    }
+.droptext p {
+  margin: 0;
+}
 
-    .fullscreen{
-        height:95%;
-    }
+.fullscreen {
+  height: 95%;
+}
 
-    .example {
-        color: v-bind('color');
-    }
+.example {
+  color: v-bind("color");
+}
 
-    /* Style the tab */
-    .tab {
-      overflow: hidden;
-      border: 1px solid #ccc;
-      background-color: #f1f1f1;
-    }
-    
-    /* Style the buttons that are used to open the tab content */
-    .tab button {
-      background-color: inherit;
-      float: left;
-      border: none;
-      outline: none;
-      cursor: pointer;
-      padding: 14px 16px;
-      transition: 0.3s;
-    }
-    
-    /* Change background color of buttons on hover */
-    .tab button:hover {
-      background-color: #ddd;
-    }
-    
-    /* Create an active/current tablink class */
-    .tab button.active {
-      background-color: #ccc;
-    }
-    
-    /* Style the tab content */
-    .tabcontent {
-      position: relative;      
-      padding: 6px 12px;
-      border: 1px solid #ccc;
-      border-top: none;
-      height:90%;
-      overflow:scroll
-    }
-    input[type=range] {
-        width:99%;
-    }
+/* Style the tab */
+.tab {
+  overflow: hidden;
+  border: 1px solid #ccc;
+  background-color: #f1f1f1;
+}
 
-  
+/* Style the buttons that are used to open the tab content */
+.tab button {
+  background-color: inherit;
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 14px 16px;
+  transition: 0.3s;
+}
+
+/* Change background color of buttons on hover */
+.tab button:hover {
+  background-color: #ddd;
+}
+
+/* Create an active/current tablink class */
+.tab button.active {
+  background-color: #ccc;
+}
+
+/* Style the tab content */
+.tabcontent {
+  position: relative;
+  padding: 6px 12px;
+  border: 1px solid #ccc;
+  border-top: none;
+  height: 90%;
+  overflow: scroll;
+}
+input[type="range"] {
+  width: 99%;
+}
 </style>
--- vue/render.vue
+++ vue/render.vue
@@ -17,137 +17,138 @@
 
 <template>
   <div>
-   <div class="render" id="render-div"></div>
-   <p>ISDs generated: {{tmax}}</p>
-   <div><span>{{selectedTime}}  </span><input type="range" min=0 v-bind:max="tmax" v-model="timeindex"></div>
-   <div class="xml"><pre v-html="xmlDisplay"></pre></div>
+    <div class="render" :id="renderId"></div>
+    <p>ISDs generated: {{ tmax }}</p>
+    <div>
+      <span>{{ selectedTime }} </span><span>Rendered using: {{imscVersion}}</span><input type="range" min="0" v-bind:max="tmax" v-model="timeindex" />
+    </div>
+    <div class="xml"><pre v-html="xmlDisplay"></pre></div>
   </div>
 </template>
 
 <script>
-
-  module.exports = {
-    data: ()=>{
-      return {
-        tmax:100,
-        timeindex:0,
-        xmlDisplay:'none',
-        selectedTime:'xx:xx:xx:xxx',
-      }
-    },
-    watch:{
-      timeindex(newt, oldt){
-        this.vdiv = document.getElementById('render-div');
-        if (this.vdiv){
-          let isd = window.imsc.generateISD(this.doc, this.t[newt]);
-          if (isd.contents.length){
-            for (let i = 0; i < this.intimes.length; i++){
-              if (this.intimes[i] >= this.t[newt]){
-                this.xmlDisplay = this.toHtmlEntities(this.divs[i]);
-                break;
-              }
+module.exports = {
+  data: () => {
+    return {
+      tmax: 100,
+      timeindex: 0,
+      xmlDisplay: "none",
+      selectedTime: "xx:xx:xx:xxx",
+      renderId:'render',
+      imscVersion: 'https://unpkg.com/imsc@1.1.0-beta.2/build/umd/imsc.all.min.js',
+    };
+  },
+  watch: {
+    timeindex(newt, oldt) {
+      this.vdiv = document.getElementById(this.renderId);
+      if (this.vdiv) {
+        let isd = this.imsc.generateISD(this.doc, this.t[newt]);
+        if (isd.contents.length) {
+          for (let i = 0; i < this.intimes.length; i++) {
+            if (this.intimes[i] >= this.t[newt]) {
+              this.xmlDisplay = this.toHtmlEntities(this.divs[i]);
+              break;
             }
-          } else {
-            this.xmlDisplay = 'none';
           }
-          this.selectedTime = this.toTime(this.t[newt]);
-          this.vdiv.innerHTML = '';
-          //(isd, element, imgResolver, eheight, ewidth, displayForcedOnlyMode, errorHandler, previousISDState, enableRollUp)
-          // force size as our div is not on screen for first render
-          window.imsc.renderHTML(isd, this.vdiv, null, 480, 854);
         } else {
-          setTimeout(()=>{
-            this.timeindex = oldt;
-            this.timeindex = newt;
-          }, 200);
+          this.xmlDisplay = "none";
         }
+        this.selectedTime = this.toTime(this.t[newt]);
+        this.vdiv.innerHTML = "";
+        //(isd, element, imgResolver, eheight, ewidth, displayForcedOnlyMode, errorHandler, previousISDState, enableRollUp)
+        // force size as our div is not on screen for first render
+        this.imsc.renderHTML(isd, this.vdiv, null, 480, 854);
+      } else {
+        setTimeout(() => {
+          this.timeindex = oldt;
+          this.timeindex = newt;
+        }, 200);
       }
     },
-    methods:{
-
-      toHtmlEntities(txt) {
-        return txt.replace(/./gm, (s)=>{
-          return (s.match(/[a-z0-9\s]+/i)) ? s : "&#" + s.charCodeAt(0) + ";";
-        });
-      },
-
-      toTime(t){
-        let hh = ('0'+((t/60/60)>>0)).slice(-2);
-        let mm = ('0'+(((t/60)>>0)%60)).slice(-2);
-        let ss = ('0'+((t>>0)%60)).slice(-2);
-        let ttt = ('00'+(((t*1000)>>0)%1000)).slice(-3);
-        return `${hh}:${mm}:${ss}.${ttt}`;
-      },
-
-      // defer processing until the DOM is populated
-      processXml(file, xml){
-        this.$nextTick(function() {
-          this.processXmlReal(file, xml);
-        });
-      },
-
-      processXmlReal(file, xml){
-        console.log('would render');
-        this.getDivs(xml);
-        this.doc = window.imsc.fromXML(xml);
-        this.t = this.doc.getMediaTimeEvents();
-        let isd = window.imsc.generateISD(this.doc, this.t[1]);
-        this.tmax = this.t.length - 1;
-
-        this.timeindex = 0;
-        this.timeindex = 1;
-        //this.vdiv.innerHTML = '';
-        //window.imsc.renderHTML(isd, this.vdiv);
-      },
-
-
-      getDivs(xml){
-        let divs = xml.match(/<div[\s\S]*?div>/g);
-
-        this.intimes = [];
-        for (let i = 0; i < divs.length; i++){
-          let b = divs[i].split('begin="')[1];
-          let times = b.split(':');
-          let hours = parseInt(times[0]);
-          let mins = parseInt(times[1]);
-          let secs = parseFloat(times[2]);
-          let t = hours*60*60 + mins*60 + secs;
-          this.intimes.push(t);
-        }
-        this.divs = divs;
-
-        console.log(divs);
-      },
-
+  },
+  methods: {
+    init(options){
+      this.imsc = options.imsc;
+      this.renderId = options.name+'-div';
+      this.imscVersion = options.version;
     },
 
-
-    mounted (){
-      this.$emit("mounted", 'render', this);
-      console.log('mounted render');
+    toHtmlEntities(txt) {
+      return txt.replace(/./gm, (s) => {
+        return s.match(/[a-z0-9\s]+/i) ? s : "&#" + s.charCodeAt(0) + ";";
+      });
     },
-    destroyed(){
-      clearInterval(this.interval);
-    }
 
-  }
+    toTime(t) {
+      let hh = ("0" + ((t / 60 / 60) >> 0)).slice(-2);
+      let mm = ("0" + (((t / 60) >> 0) % 60)).slice(-2);
+      let ss = ("0" + ((t >> 0) % 60)).slice(-2);
+      let ttt = ("00" + (((t * 1000) >> 0) % 1000)).slice(-3);
+      return `${hh}:${mm}:${ss}.${ttt}`;
+    },
+
+    // defer processing until the DOM is populated
+/*    processXml(file, xml) {
+      this.$nextTick(function () {
+        this.processXmlReal(file, xml);
+      });
+    },*/
+
+    processXml(file, xml) {
+      //console.log("would render");
+      this.getDivs(xml);
+      this.doc = this.imsc.fromXML(xml);
+      this.t = this.doc.getMediaTimeEvents();
+      this.tmax = this.t.length - 1;
+      this.timeindex = 0;
+      this.timeindex = 1;
+      //this.vdiv.innerHTML = '';
+      //window.imsc.renderHTML(isd, this.vdiv);
+    },
+
+    getDivs(xml) {
+      let divs = xml.match(/<div[\s\S]*?div>/g);
+
+      this.intimes = [];
+      for (let i = 0; i < divs.length; i++) {
+        let b = divs[i].split('begin="')[1];
+        let times = b.split(":");
+        let hours = parseInt(times[0]);
+        let mins = parseInt(times[1]);
+        let secs = parseFloat(times[2]);
+        let t = hours * 60 * 60 + mins * 60 + secs;
+        this.intimes.push(t);
+      }
+      this.divs = divs;
+
+      //console.log(divs);
+    },
+  },
+
+  mounted() {
+    this.$emit("mounted", "render", this);
+    //console.log("mounted render");
+  },
+  destroyed() {
+    clearInterval(this.interval);
+  },
+};
 //@ sourceURL=/vue/render.vue
 </script>
 
 <style scoped>
-  .render {
-    border: 1px solid black;
-    height:480px; 
-    width:854px;
-    background-color: darkgray;
-  }
-  .xml {
-    max-height: 25vh;
-    overflow:scroll;
-    border: 2px solid black;
-  }
-  h2 {
-    margin-bottom: 0;
-  }
-
+.render {
+  border: 1px solid black;
+  height: 480px;
+  width: 854px;
+  background-color: darkgray;
+}
+.xml {
+  max-height: 25vh;
+  overflow: scroll;
+  border: 2px solid black;
+}
+h2 {
+  margin-bottom: 0;
+}
 </style>
